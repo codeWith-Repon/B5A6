1a0bea760684d461361a03cf7c907b8b348f4c21
Md Repon <repon7253@gmail.com>
2025-08-21 15:11:07 +0600

    sticky navbar

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -13,6 +13,7 @@ import {
 } from '@/components/ui/popover';
 import { Link } from 'react-router';
 import { ModeToggle } from './ModeToggler';
+import { useEffect, useState } from 'react';
 
 // Navigation links array to be used in both desktop and mobile menus
 const navigationLinks = [
@@ -25,88 +26,106 @@ const navigationLinks = [
 ];
 
 export default function Navbar() {
+  const [isSticky, setIsSticky] = useState<boolean>(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setIsSticky(window.scrollY > 100);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
   return (
-    <header className='border-b px-4 md:px-6 '>
-      <div className='flex h-16 items-center justify-between gap-4 mx-auto container'>
-        {/* Left side */}
-        <div className='flex items-center gap-2'>
-          {/* Mobile menu trigger */}
-          <Popover>
-            <PopoverTrigger asChild>
-              <Button
-                className='group size-8 md:hidden'
-                variant='ghost'
-                size='icon'
-              >
-                <svg
-                  className='pointer-events-none'
-                  width={16}
-                  height={16}
-                  viewBox='0 0 24 24'
-                  fill='none'
-                  stroke='currentColor'
-                  strokeWidth='2'
-                  strokeLinecap='round'
-                  strokeLinejoin='round'
-                  xmlns='http://www.w3.org/2000/svg'
+    <header
+      className={`border-b px-4 md:px-6 transition-all duration-500 top-0 z-40 bg-background  w-full ${
+        isSticky ? 'sticky bg-background/80 backdrop-blur-md shadow-sm' : ''
+      }`}
+    >
+      <div className={`sticky`}>
+        <div className='flex h-16 items-center justify-between gap-4 mx-auto container max-w-[1370px]'>
+          {/* Left side */}
+          <div className='flex items-center gap-2'>
+            {/* Mobile menu trigger */}
+            <Popover>
+              <PopoverTrigger asChild>
+                <Button
+                  className='group size-8 md:hidden'
+                  variant='ghost'
+                  size='icon'
                 >
-                  <path
-                    d='M4 12L20 12'
-                    className='origin-center -translate-y-[7px] transition-all duration-300 ease-[cubic-bezier(.5,.85,.25,1.1)] group-aria-expanded:translate-x-0 group-aria-expanded:translate-y-0 group-aria-expanded:rotate-[315deg]'
-                  />
-                  <path
-                    d='M4 12H20'
-                    className='origin-center transition-all duration-300 ease-[cubic-bezier(.5,.85,.25,1.8)] group-aria-expanded:rotate-45'
-                  />
-                  <path
-                    d='M4 12H20'
-                    className='origin-center translate-y-[7px] transition-all duration-300 ease-[cubic-bezier(.5,.85,.25,1.1)] group-aria-expanded:translate-y-0 group-aria-expanded:rotate-[135deg]'
-                  />
-                </svg>
-              </Button>
-            </PopoverTrigger>
-            <PopoverContent align='start' className='w-36 p-1 md:hidden'>
-              <NavigationMenu className='max-w-none *:w-full'>
-                <NavigationMenuList className='flex-col items-start gap-0 md:gap-2'>
+                  <svg
+                    className='pointer-events-none'
+                    width={16}
+                    height={16}
+                    viewBox='0 0 24 24'
+                    fill='none'
+                    stroke='currentColor'
+                    strokeWidth='2'
+                    strokeLinecap='round'
+                    strokeLinejoin='round'
+                    xmlns='http://www.w3.org/2000/svg'
+                  >
+                    <path
+                      d='M4 12L20 12'
+                      className='origin-center -translate-y-[7px] transition-all duration-300 ease-[cubic-bezier(.5,.85,.25,1.1)] group-aria-expanded:translate-x-0 group-aria-expanded:translate-y-0 group-aria-expanded:rotate-[315deg]'
+                    />
+                    <path
+                      d='M4 12H20'
+                      className='origin-center transition-all duration-300 ease-[cubic-bezier(.5,.85,.25,1.8)] group-aria-expanded:rotate-45'
+                    />
+                    <path
+                      d='M4 12H20'
+                      className='origin-center translate-y-[7px] transition-all duration-300 ease-[cubic-bezier(.5,.85,.25,1.1)] group-aria-expanded:translate-y-0 group-aria-expanded:rotate-[135deg]'
+                    />
+                  </svg>
+                </Button>
+              </PopoverTrigger>
+              <PopoverContent align='start' className='w-36 p-1 md:hidden'>
+                <NavigationMenu className='max-w-none *:w-full'>
+                  <NavigationMenuList className='flex-col items-start gap-0 md:gap-2'>
+                    {navigationLinks.map((link, index) => (
+                      <NavigationMenuItem key={index} className='w-full'>
+                        <NavigationMenuLink href={link.href} className='py-1.5'>
+                          {link.label}
+                        </NavigationMenuLink>
+                      </NavigationMenuItem>
+                    ))}
+                  </NavigationMenuList>
+                </NavigationMenu>
+              </PopoverContent>
+            </Popover>
+            {/* Main nav */}
+            <div className='flex items-center gap-6'>
+              <Link to='/' className='text-primary hover:text-primary/90'>
+                <Logo />
+              </Link>
+              {/* Navigation menu */}
+              <NavigationMenu className='max-md:hidden'>
+                <NavigationMenuList className='gap-2'>
                   {navigationLinks.map((link, index) => (
-                    <NavigationMenuItem key={index} className='w-full'>
-                      <NavigationMenuLink href={link.href} className='py-1.5'>
+                    <NavigationMenuItem key={index}>
+                      <NavigationMenuLink
+                        href={link.href}
+                        className='text-muted-foreground hover:text-primary py-1.5 font-medium'
+                      >
                         {link.label}
                       </NavigationMenuLink>
                     </NavigationMenuItem>
                   ))}
                 </NavigationMenuList>
               </NavigationMenu>
-            </PopoverContent>
-          </Popover>
-          {/* Main nav */}
-          <div className='flex items-center gap-6'>
-            <Link to='/' className='text-primary hover:text-primary/90'>
-              <Logo />
-            </Link>
-            {/* Navigation menu */}
-            <NavigationMenu className='max-md:hidden'>
-              <NavigationMenuList className='gap-2'>
-                {navigationLinks.map((link, index) => (
-                  <NavigationMenuItem key={index}>
-                    <NavigationMenuLink
-                      href={link.href}
-                      className='text-muted-foreground hover:text-primary py-1.5 font-medium'
-                    >
-                      {link.label}
-                    </NavigationMenuLink>
-                  </NavigationMenuItem>
-                ))}
-              </NavigationMenuList>
-            </NavigationMenu>
+            </div>
           </div>
-        </div>
-        {/* Right side */}
-        <div className='flex items-center gap-2'>
-          <ModeToggle />
-          <Button asChild size='sm' className='text-sm'>
-            <Link to={'/sign-in'}>Sign In</Link>
-          </Button>
+          {/* Right side */}
+          <div className='flex items-center gap-2'>
+            <ModeToggle />
+            <Button asChild size='sm' className='text-sm'>
+              <Link to={'/sign-in'}>Sign In</Link>
+            </Button>
+          </div>
         </div>
       </div>
     </header>
