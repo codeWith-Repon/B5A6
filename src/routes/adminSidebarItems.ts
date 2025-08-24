4133043b4d673c08663f9663804a6f8b5f02b61d
Md Repon <repon7253@gmail.com>
2025-08-24 21:35:26 +0600

    split side bar item into another file

---
+++ src/routes/adminSidebarItems.ts
@@ -0,0 +1,36 @@
+export const adminSidebarItems = {
+    navMain: [
+        {
+            title: "Dashboard",
+            url: "#",
+            items: [
+                {
+                    title: "Installation",
+                    url: "/admin/analytics",
+                }
+            ],
+        },
+        {
+            title: "Drivers",
+            url: "#",
+            items: [
+                {
+                    title: "All Drivers",
+                    url: "/admin/all-drivers",
+                },
+                {
+                    title: "Approved Drivers",
+                    url: "/admin/approved-drivers",
+                },
+                {
+                    title: "Pending Drivers",
+                    url: "/admin/pending-drivers",
+                },
+                {
+                    title: "Suspended Drivers",
+                    url: "/admin/suspended-drivers",
+                },
+            ],
+        }
+    ],
+}
