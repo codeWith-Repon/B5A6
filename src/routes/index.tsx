0f36a53d00efae3da94b67eb1c1ee5eadbd515c2
Md Repon <repon7253@gmail.com>
2025-08-24 23:48:21 +0600

    fix issue

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -7,7 +7,7 @@ import Login from '@/pages/Login';
 import Register from '@/pages/Register';
 import Verify from '@/pages/Verify';
 import { generateRoutes } from '@/utils/generateRoutes';
-import { createBrowserRouter } from 'react-router';
+import { createBrowserRouter, Navigate } from 'react-router';
 import { adminSidebarItems } from './adminSidebarItems';
 import { driverSidebarItems } from './DriverSidebarItem';
 import { userSidebarItems } from './userSidebarItems';
@@ -34,17 +34,26 @@ export const router = createBrowserRouter([
   {
     Component: DashboardLayout,
     path: '/admin',
-    children: [...generateRoutes(adminSidebarItems)],
+    children: [
+      { index: true, element: <Navigate to='/admin/analytics' /> },
+      ...generateRoutes(adminSidebarItems),
+    ],
   },
   {
     Component: DashboardLayout,
     path: '/driver',
-    children: [...generateRoutes(driverSidebarItems)],
+    children: [
+      { index: true, element: <Navigate to='/driver/ride-request' /> },
+      ...generateRoutes(driverSidebarItems),
+    ],
   },
   {
     Component: DashboardLayout,
     path: '/rider',
-    children: [...generateRoutes(userSidebarItems)],
+    children: [
+      { index: true, element: <Navigate to='/rider/ride-request' /> },
+      ...generateRoutes(userSidebarItems),
+    ],
   },
   {
     Component: Login,
